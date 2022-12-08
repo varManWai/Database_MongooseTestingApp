b5c3828c193a236848d7aae8c0b2e9d84985aa74
varManWai <laizoke98@gmail.com>
2022-12-09 02:15:17 +0800

    clean something

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-console.log(process.env.MONGODB_URI);
-
 const connectMongo = async () => mongoose.connect(process.env.MONGODB_URI);
 
 export default connectMongo;
