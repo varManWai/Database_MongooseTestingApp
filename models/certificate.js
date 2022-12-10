331dd753f101796727cc836f8b6aa6b1ca6aac69
varManWai <laizoke98@gmail.com>
2022-12-11 01:13:57 +0800

    added certificate schema model

---
+++ models/certificate.js
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const certificateSchema = new Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  dateIssued: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const Certificate =
+  models.Certificate || model("Certificate", certificateSchema);
+
+export default Certificate;
